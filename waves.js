c3a4de2cebead0210df481390840683d38afcbf4
tomyeezy <114565413+tomyebest@users.noreply.github.com>
2023-12-18 16:05:46 +1000

    yes

--- waves.js
+++ waves.js
@@ -21,30 +21,6 @@ getWaves();
 
 // ------------------- //
 
-
-// FETCH WINDS FROM API //
-
-async function getWinds() {
-  const response = await fetch(
-    "https://api.open-meteo.com/v1/gfs?latitude=-26.6033&longitude=153.091&current=wind_speed_10m&wind_speed_unit=kn&forecast_days=1"
-  );
-  if (!response.ok) {
-    throw new Error("HTTP error " + response.status);
-  }
-  const winds = await response.json();
-
-  // log to console
-  console.log(winds);
-
-  // return addData function
-  addData(winds);
-}
-
-// return function
-getWinds();
-
-// ------------------- //
-
 // CONVERT WAVE DIRECTION FROM DEGREES TO NORTH-EAST-SOUTH-WEST //
 
 function convertWaveDirection(waves) {
